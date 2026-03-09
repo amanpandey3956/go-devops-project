2de9c06d572d9fb4fdaf00ae7504ac04f20a7f00
amanpandey3956 <amanpandey39563@gmail.com>
2026-03-09 21:30:21 +0530

    first commit

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "static/home.html")
+}
+
+func projectsPage(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "static/projects.html")
+}
+
+func aboutPage(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "static/about.html")
+}
+
+func contactPage(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "static/contact.html")
+}
+
+func main() {
+
+	http.HandleFunc("/", homePage)
+	http.HandleFunc("/projects", projectsPage)
+	http.HandleFunc("/about", aboutPage)
+	http.HandleFunc("/contact", contactPage)
+
+	log.Println("Server running on port 8080")
+
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
